93f683b6985b34b29758e0a1a2674e0f5e6e8765
ptilak5210-gif <ptilak5210@gmail.com>
2025-12-19 17:50:14 +0000

    conversion of datatype

---
+++ basic/03_conversionoperator.js
@@ -0,0 +1,42 @@
+let score= 33
+let score2="22"
+let score3="33abc"
+let score4=true
+
+
+console.log(typeof score);
+console.log(typeof (score));
+console.log(typeof score4);
+
+console.log(typeof score2);
+console.log(typeof (score2));
+
+console.log(typeof score3);
+
+let valueinnum=Number(score);
+let valueinnum2 =Number(score2);
+let valueinnum3 =Number(score3);
+let valueinnum4 =Number(score4);
+
+
+
+console.log(typeof valueinnum);
+console.log(typeof valueinnum2);
+console.log(typeof valueinnum3);
+console.log( valueinnum3); //NaN output no print "33abc"
+console.log(typeof valueinnum4);
+console.log(score4);
+
+let somenum = 33
+let stringnumber =String(somenum)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
+
+
+
+
+

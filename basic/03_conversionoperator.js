71ab4e1a5a1ececd747394012d6e0eb42cae9364
ptilak5210-gif <ptilak5210@gmail.com>
2025-12-19 18:23:47 +0000

    operatirs

--- basic/03_conversionoperator.js
+++ basic/03_conversionoperator.js
@@ -4,14 +4,14 @@ let score3="33abc"
 let score4=true
 
 
-console.log(typeof score);
-console.log(typeof (score));
-console.log(typeof score4);
+// console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof score4);
 
-console.log(typeof score2);
-console.log(typeof (score2));
+// console.log(typeof score2);
+// console.log(typeof (score2));
 
-console.log(typeof score3);
+// console.log(typeof score3);
 
 let valueinnum=Number(score);
 let valueinnum2 =Number(score2);
@@ -19,18 +19,47 @@ let valueinnum3 =Number(score3);
 let valueinnum4 =Number(score4);
 
 
-
-console.log(typeof valueinnum);
-console.log(typeof valueinnum2);
-console.log(typeof valueinnum3);
-console.log( valueinnum3); //NaN output no print "33abc"
-console.log(typeof valueinnum4);
-console.log(score4);
+// console.log(typeof valueinnum);
+// console.log(typeof valueinnum2);
+// console.log(typeof valueinnum3);
+// console.log( valueinnum3); //NaN output no print "33abc"
+// console.log(typeof valueinnum4);
+// console.log(score4);
 
 let somenum = 33
 let stringnumber =String(somenum)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// *****************operations********************
+
+let value = 3
+let negvalue= -value
+console.log(negvalue);
+
+// *****arithmetic operators*****
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2**3); //power
+// console.log(2%3); //modulus
+
+// console.log(1 + "2");       //12
+// console.log("1" + 2);       //12
+// console.log("1" + "2");     //12
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + 2 + "2");   //32
+// console.log(+true);         //1
+// console.log(true+);         //error
+// console.log(+"");         //0
+
+
+let numcount = 100;
+numcount++ ;    //postfix
+// ++numcount ;    //prifiex
+console.log(numcount);
+
 
 
 
